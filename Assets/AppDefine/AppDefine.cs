a847b3cfec580f1ceed2d5589135db9d26bf8520
joey1258 <1260216329@qq.com>
2016-10-08 13:25:26 +0800

    2016.10.8
    fix

---
+++ Assets/AppDefine/AppDefine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AppDefine
+{
+    /// <summary>
+    /// 场景根物体名称（切换场景不保留）
+    /// </summary>
+    public static string RootObjectName = "SceneRoot";
+
+    /// <summary>
+    /// 切换场景时不进行销毁的根物体名称
+    /// </summary>
+    public static string DDOLRootObjectName = "DDOLRoot";
+
+    /// <summary>
+    /// 应用程序名称
+    /// </summary>
+    public const string AppName = "SimpleContainer";
+
+    /// <summary>
+    /// 应用程序前缀
+    /// </summary>
+    public const string AppPrefix = AppName + "_";
+
+    /// <summary>
+    /// Streaming 目录 
+    /// </summary>
+    public const string AssetDir = "StreamingAssets";
+}
